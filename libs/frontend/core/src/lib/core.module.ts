4ee43c6bd82dc748131d79f1441d9a04eddf0e77
Gena Kulabukhov <genabush1990@gmail.com>
2023-06-15 09:01:50 +0300

    added ngrx store

--- libs/frontend/core/src/lib/core.module.ts
+++ libs/frontend/core/src/lib/core.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// '@ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 // services
 import { AuthService, ProductsService, UserService } from './services';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
   providers: [UserService, AuthService, ProductsService],
 })
 export class CoreModule {}
